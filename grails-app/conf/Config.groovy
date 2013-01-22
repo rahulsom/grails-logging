4dd39cdf5cb005e9b144573146456a5ee85c9ae7
jr4 <jon@ivydesign.com>
2013-01-22 14:49:21 -0800

    add config-plugin, admin controller

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,10 +1,29 @@
-// configuration for plugin testing - will not be included in the plugin zip
-import org.apache.log4j.Level
+// configuration for plugin testing - will not be included in the packaged plugin
+
 import logging.AnsiConsoleAppender
 
 log4j = {
   appenders {
-    appender new AnsiConsoleAppender(name:'stdout', threshold: Level.ALL,
-        layout:pattern(conversionPattern: '%d{ISO8601} [%15.15t] %-5p %30.30c - %m%n'))
+    appender new AnsiConsoleAppender(
+      name: 'stdout', 
+      layout: pattern(conversionPattern: '%d{ISO8601} [%15.15t] %-5p %30.30c - %m%n')
+    )
+  }
+  error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
+         'org.codehaus.groovy.grails.web.pages', //  GSP
+         'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+         'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+         'org.codehaus.groovy.grails.web.mapping', // URL mapping
+         'org.codehaus.groovy.grails.commons', // core / classloading
+         'org.codehaus.groovy.grails.plugins', // plugins
+         'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+         'org.springframework',
+         'org.hibernate',
+         'net.sf.ehcache.hibernate'
+
+  warn   'org.mortbay.log'
+
+  root {
+    warn 'stdout'
   }
 }
